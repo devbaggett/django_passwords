34540cd3dee223bc40587c67dff01a9ba01fd60a
devbaggett <devabaggett@gmail.com>
2018-07-31 19:41:29 -0700

    coded user models and forms

--- apps/users_app/models.py
+++ apps/users_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfileInfo(models.Model):
+    # extends class and allows us to add more attributes to actual user
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # additional classes
+    portfolio_site = models.URLField(blank=True)
+
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+
+    def __str__(self):
+    	# username is attribute of user object now
+    	return self.user.username
